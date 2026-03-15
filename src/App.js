5aa5d1fd4ce0e138a604a8f995b939e37956ac35
Abkour Douaa <abkourdouaa@gmail.com>
2026-03-15 16:14:14 +0000

    contact page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { Toaster } from 'sonner';
+import ContactPage from "./page/ContactPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Toaster position="top-right" richColors />
+         <ContactPage />
     </div>
   );
 }
